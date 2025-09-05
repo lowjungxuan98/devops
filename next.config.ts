a938b9d30baa2b7a9068d585a00831a2df457123
lowjungxuan <lowjungxuan@gmail.com>
2025-09-05 10:45:03 +0800

    Refactor next.config.ts to remove experimental server components and update CI workflow with additional image metadata labels

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,5 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
-  server: {
-    hostname: '0.0.0.0',
-    port: 3000,
-  },
 };
-
 export default nextConfig;
